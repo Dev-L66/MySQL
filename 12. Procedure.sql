f8840573d713c99ed0721be85b05aa6b4b6bdf6b
Dev-L66 <sabeebashil@yahoo.com>
2026-08-01 02:58:13 +0300

    trigger events

---
+++ 12. Procedure.sql
@@ -0,0 +1,33 @@
+-- Stored Procedure
+
+CREATE PROCEDURE large_salaries()
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+CALL large_salaries();
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries3()
+BEGIN
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+SELECT *
+FROM employee_salary
+WHERE salary >= 10000;
+END $$
+DELIMITER ;
+
+CALL large_salaries3();
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries4(empid INT)
+BEGIN
+SELECT *
+FROM employee_salary
+WHERE employee_id = empid;
+END $$
+DELIMITER ;
+
+CALL large_salaries4(1);
